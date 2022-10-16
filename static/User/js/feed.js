374194555e1cb14158b5e78083c79b6c2507c589
Ashmit <ashmit454@gmail.com>
2022-10-16 12:00:26 +0530

    added upload media feature.

--- static/User/js/feed.js
+++ static/User/js/feed.js
@@ -1,6 +1,9 @@
 const post = document.getElementById("post-modal");
 const createModal = document.getElementById("post-create-modal");
 const closeButton = document.getElementById("close-button");
+const formFile = document.getElementById("formFile");
+
+const applicableFileTypes = ['jpg', 'jpeg', 'mp4', 'x-m4v'];
 
 post.onclick = function() {
     createModal.style.display = "block";
@@ -8,4 +11,14 @@ post.onclick = function() {
 
 closeButton.onclick = function() {
     createModal.style.display = "none";
+}
+
+formFile.onchange = function() {
+    filepath = formFile.value.split(".");
+    type = filepath[filepath.length - 1];
+
+    if (!applicableFileTypes.includes(type)) {
+        alert('File Type not applicable');
+        formFile.value = "";
+    }
 }
