af28ac19b095f4bb87306a65c2def9bafec235cb
Ashmit <ashmit454@gmail.com>
2022-10-17 22:09:56 +0530

    added preview image feature.

--- static/User/js/feed.js
+++ static/User/js/feed.js
@@ -1,7 +1,8 @@
 const post = document.getElementById("post-modal");
 const createModal = document.getElementById("post-create-modal");
 const closeButton = document.getElementById("close-button");
-const formFile = document.getElementById("formFile");
+const mediaFile = document.getElementById("mediaFile");
+const image = document.getElementById('image');
 
 const applicableFileTypes = ['jpg', 'jpeg', 'mp4', 'x-m4v'];
 
@@ -11,14 +12,22 @@ post.onclick = function() {
 
 closeButton.onclick = function() {
     createModal.style.display = "none";
+    mediaFile.value = "";
+    image.src = "";
 }
 
-formFile.onchange = function() {
-    filepath = formFile.value.split(".");
+mediaFile.onchange = function(evt) {
+    filepath = mediaFile.value.split(".");
     type = filepath[filepath.length - 1];
 
     if (!applicableFileTypes.includes(type)) {
         alert('File Type not applicable');
-        formFile.value = "";
+        mediaFile.value = "";
     }
+    previewImage(evt);
+}
+
+function previewImage(event){
+    const media = URL.createObjectURL(event.target.files[0]);
+    image.src = media;
 }
